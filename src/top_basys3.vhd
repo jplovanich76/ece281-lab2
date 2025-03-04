eb3ed88614f0b7e4a9634545e3fd2b9c88863f2e
jplovanich76 <c27jack.plovanich@afacademy.af.edu>
2025-03-03 22:37:32 -0700

    x

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -1,59 +1,4 @@
---+----------------------------------------------------------------------------
---| 
---| COPYRIGHT 2025 United States Air Force Academy All rights reserved.
---| 
---| United States Air Force Academy     __  _______ ___    _________ 
---| Dept of Electrical &               / / / / ___//   |  / ____/   |
---| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
---| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
---| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
---| 
---| ---------------------------------------------------------------------------
---|
---| FILENAME      : top_basys3.vhd
---| AUTHOR(S)     : Capt Phillip Warner & Capt Brian Yarbrough
---| CREATED       : 01/22/2018 Last modified 02/18/2025
---| DESCRIPTION   : This file implements the top level module for a BASYS 3 to utilize 
---|					a seven-segment decoder for displaying hex values on seven-segment 
---|					displays (7SD) according to 4-bit inputs provided by switches.
---|
---|					Inputs:  sw (3:0)  --> 4-bit signal to deternmine 7SD value to be diplayed
---|							 btnC	   --> activate 7SD
---|
---|					Output:  seg (6:0) --> 7-bit signal to activate the individual segments (active low)
---|							 an (3:0)  --> 4-bit signal to control which display turns on (active low)
---|
---|
---+----------------------------------------------------------------------------
---|
---| REQUIRED FILES :
---|
---|    Libraries : ieee
---|    Packages  : std_logic_1164, numeric_std
---|    Files     : sevenSegDecoder.vhd
---|
---+----------------------------------------------------------------------------
---|
---| NAMING CONVENSIONS :
---|
---|    xb_<port name>           = off-chip bidirectional port ( _pads file )
---|    xi_<port name>           = off-chip input port         ( _pads file )
---|    xo_<port name>           = off-chip output port        ( _pads file )
---|    b_<port name>            = on-chip bidirectional port
---|    i_<port name>            = on-chip input port
---|    o_<port name>            = on-chip output port
---|    c_<signal name>          = combinatorial signal
---|    f_<signal name>          = synchronous signal
---|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
---|    <signal name>_n          = active low signal
---|    w_<signal name>          = top level wiring signal
---|    g_<generic name>         = generic
---|    k_<constant name>        = constant
---|    v_<variable name>        = variable
---|    sm_<state machine type>  = state machine type definition
---|    s_<signal name>          = state name
---|
---+----------------------------------------------------------------------------
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
@@ -79,23 +24,27 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design
-
-
-  -- create wire to connect button to 7SD enable (active-low)
+  component sevenseg_decoder is
+    port(
+        i_Hex : in std_logic_vector(3 downto 0);
+        o_seg_n : out std_logic_vector(6 downto 0)
+        );
+        end component;
+ 
+      signal seg_output : std_logic_vector(6 downto 0);
 
   
 begin
-	-- PORT MAPS ----------------------------------------
 
-	--	Port map: wire your component up to the switches and seven-segment display cathodes
-	-----------------------------------------------------	
-	
-	
-	-- CONCURRENT STATEMENTS ----------------------------
-	
-	-- wire up active-low 7SD anode (active low) to button (active-high)
-	-- display 7SD 0 only when button pushed
-	-- other 7SD are kept off
-	-----------------------------------------------------
-	
+    -- Instantiate the sevenseg_decoder
+    sevenseg_inst : sevenseg_decoder
+    port map(
+        i_Hex   => sw,          
+        o_seg_n => seg_output   
+    );
+
+    seg <= seg_output;
+
+    an <= "1110" when btnC = '1' else "1111"; 
+
 end top_basys3_arch;
